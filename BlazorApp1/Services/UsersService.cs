4d54e1fee18428e3745015c75ce86c38192fa9ab
likeroge <likeroge@gmail.com>
2025-01-13 18:24:40 +0400

    init

--- BlazorApp1/Services/UsersService.cs
+++ BlazorApp1/Services/UsersService.cs
@@ -5,14 +5,16 @@ namespace BlazorApp1.Services;
 public class UsersService
 {
     private readonly HttpClient _httpClient;
+    private readonly IConfiguration _configuration;
 
-    public UsersService(HttpClient httpClient)
+    public UsersService(HttpClient httpClient, IConfiguration configuration)
     {
         _httpClient = httpClient;
+        _configuration = configuration;
     }
-    
     public IEnumerable<User>? GetUsers()
     {
+        var address = _configuration["BaseUrl"];
         return  _httpClient.GetFromJsonAsync<IEnumerable<User>>("Users").Result;
     }
 }
